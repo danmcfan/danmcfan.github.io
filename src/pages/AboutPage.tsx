c056f91b77fd99c60652716a862aeda265fe9fb9
Danny O'Brien <danmcfan33@gmail.com>
2026-03-04 16:09:49 -0800

    typo

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -9,7 +9,7 @@ export function AboutPage() {
         imageSrc="/headshot.jpeg"
         name="Danny O'Brien"
         role="Software Engineer and Systems Mage"
-        description="I build software that is reliable, performant, and asthetically pleasing. I enjoy taking vague ideas, mapping the terrain, and turning them into clear systems through thoughtful architecture and steady iteration. When I'm not in my coding dungeon, you can usually find me out in the California sun surfing or rock climbing."
+        description="I build software that is reliable, performant, and aesthetically pleasing. I enjoy taking vague ideas, mapping the terrain, and turning them into clear systems through thoughtful architecture and steady iteration. When I'm not in my coding dungeon, you can usually find me out in the California sun surfing or rock climbing."
       >
         <SocialLinks socials={socials} />
       </ProfileBlock>
