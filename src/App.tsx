af81a1eb5a3c7a4b90969487bd3c9b018ef2ee5a
Danny O'Brien <danmcfan33@gmail.com>
2026-03-21 16:03:33 -0700

    try again

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import { AboutPage } from "./pages/AboutPage";
 import { BlogPage } from "./pages/BlogPage";
 import { ProjectsPage } from "./pages/ProjectsPage";
 
-function HomePageRedirect() {
+function ProjectsRedirect() {
   return <Navigate href="/projects" />;
 }
 
 export function App() {
   return (
     <Router root={Layout}>
-      <Route path="/" component={HomePageRedirect} />
+      <Route path="/" component={ProjectsRedirect} />
       <Route path="/projects" component={ProjectsPage} />
       <Route path="/blog" component={BlogPage} />
       <Route path="/about" component={AboutPage} />
+      <Route path="*404" component={ProjectsRedirect} />
     </Router>
   );
 }
