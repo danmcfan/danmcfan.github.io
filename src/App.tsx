655053ba40ddd51058608c6f224f4391b4d21ed2
Danny O'Brien <danmcfan33@gmail.com>
2026-03-21 15:57:31 -0700

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { Route, Router } from "@solidjs/router";
 
 import { Layout } from "./layout/Layout";
 import { AboutPage } from "./pages/AboutPage";
-import { BlogPage } from "./pages/BlogPage";
 import { ProjectsPage } from "./pages/ProjectsPage";
 
 export function App() {
   return (
     <Router root={Layout}>
-      <Route path="/" component={BlogPage} />
+      <Route path="/" component={ProjectsPage} />
       <Route path="/projects" component={ProjectsPage} />
       <Route path="/about" component={AboutPage} />
     </Router>
